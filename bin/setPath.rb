c72abcd09d3df684e30565df8e0658e40dcf33b5
Liu Jun <szpapas@qq.com>
2012-03-17 00:37:18 +0800

    update setPath.js to exclude self

--- bin/setPath.rb
+++ bin/setPath.rb
@@ -1,8 +1,10 @@
 ARGV.each do |file|
   puts "processing #{file}..."
-  ss = File.open(file).read
-  ss = ss.gsub("#$:<<'/Library/", "\##$:<<'/Library/").gsub("\$:<<'/usr/", "$:<<'/usr/")
-  ff = File.open(file,'w')
-  ff.write(ss)
-  ff.close
+  if !file.include?'setPath'
+    ss = File.open(file).read
+    ss = ss.gsub("$:<<'/Library/", "\#$:<<'/Library/").gsub("\#$:<<'/usr/", "\$:<<'/usr/")
+    ff = File.open(file,'w')
+    ff.write(ss)
+    ff.close
+  end  
 end
