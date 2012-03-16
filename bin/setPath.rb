14fd8ad4819a979431a7b4c36e0b7e31dabfa1e9
Liu Jun <szpapas@qq.com>
2012-03-17 04:05:36 +0800

    modify setupPath.rb

--- bin/setPath.rb
+++ bin/setPath.rb
@@ -2,6 +2,7 @@ ARGV.each do |file|
   puts "processing #{file}..."
   if !file.include?'setPath'
     ss = File.open(file).read
+    
     ss = ss.gsub("$:<<'/Library/", "\#$:<<'/Library/").gsub("\#$:<<'/usr/", "\$:<<'/usr/")
     ff = File.open(file,'w')
     ff.write(ss)
