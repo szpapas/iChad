ecd1f64db8bf0595d7e1f60b73bfa120837f3fba
Liu Jun <szpapas@qq.com>
2012-03-27 02:48:07 +0800

    add bin/export_image.rb

--- bin/export_image.rb
+++ bin/export_image.rb
@@ -17,43 +17,44 @@ $conn = PGconn.open(:dbname=>'JY1017', :user=>'postgres', :password=>'brightechs
 
 ss = ARGV[0].split('_')
 qzh, dalb, mlh = ss[0], ss[1], ss[2]
+
+dh = "#{qzh}_#{dalb}_#{mlh}_%"
 option = ARGV[1]
 
-
-if option.to_i == 0
-  imgs = $conn.exec("select id, dh from timage where dh like '#{dh}' order by id;")
-elsif option.to_i == 1
-  imgs = $conn.exec("select id, dh from timage where dh like '#{dh}' and yxmc like '[ML|JN]%' order by id;")
-else
-  
+if option.to_i == 1
+  imgs = $conn.exec("select id from timage where dh like '#{dh}' and yxbh like 'ML%' order by id;")
+elsif option.to_i == 2        
+  imgs = $conn.exec("select id from timage where dh like '#{dh}' and yxbh like '[ML|JN]%' order by id;")
+else                          
+  imgs = $conn.exec("select id from timage where dh like '#{dh}' order by id;")
 end
 
   
-dh = "#{qzh}_#{dalb}_#{mlh}_%"
 savePath = ARGV[2].nil? ? './dady/export' : ARGV[2] 
 outPath = "#{savePath}/#{qzh}/#{mlh}"
 
-if File.exists?(outPath)
-  system "rm -rf #{outPath}"
+puts "check path #{outPath}..."
+if !File.exists?(outPath)
+  puts "create path #{outPath}..."
+  system "mkdir -p #{outPath}"
 end 
-system "mkdir -p #{outPath}"
-
 
 for k in 0..imgs.count - 1 do
   dd = imgs[k]
   user = $conn.exec("select * from timage where id='#{dd['id']}';")
+  prefix = user[0]["yxmc"][0..-10]
+  if !File.exists?("#{outPath}/#{prefix}")
+    puts "create path #{outPath}/#{prefix}..."
+    system "mkdir -p '#{outPath}/#{prefix}'"
+  end
+
   ss=PGconn.unescape_bytea(ss = user[0]["data"]) 
-  local_filename = "#{outPath}/"+user[0]["yxmc"]
+  local_filename = "#{outPath}/#{prefix}/"+user[0]["yxbh"]
+  puts "save to #{local_filename} ..."
   File.open(local_filename, 'w') {|f| f.write(ss) }
 end
 
-if !ARGV[3].nil?
-  system "rm -rf #{outPath}.zip"
-  puts "zip #{outPath}.zip #{outPath}/*"
-  system "zip #{outPath}.zip #{outPath}/*"
-end
-
 puts " Completed in #{Time.now-t1} s" 
-puts "***==export finished: #{outPath}.zip"
+puts "***==export finished: #{outPath}"
 
 $conn.close
