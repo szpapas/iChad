faca535de277df0ba11545c66e70c1da128942ef
Liu Jun <szpapas@qq.com>
2012-03-27 17:55:49 +0800

    change import image to adate ml to JN

--- tz_data2.sh
+++ tz_data2.sh
@@ -59,8 +59,8 @@ ruby ./dady/bin/import_image.rb 4 19 10 /mnt/data1/TZ/19/ -- 147180
 ruby ./dady/bin/import_image.rb 4 40 3 /mnt/data1/TZ/40/  -- 10745
 ruby ./dady/bin/import_image.rb 4 41 3 /mnt/data1/TZ/41/  -- 10604
 ruby ./dady/bin/import_image.rb 4 42 3 /mnt/data1/TZ/42/  -- 5608
-ruby ./dady/bin/import_image.rb 4 43 3 /mnt/data1/TZ/43/  -- 8919
-ruby ./dady/bin/import_image.rb 4 44 3 /mnt/data1/TZ/44/  -- 4123
+ruby ./dady/bin/import_image.rb 4 43 3 /mnt/data1/TZ/43/  -- 8919       --pass
+ruby ./dady/bin/import_image.rb 4 44 3 /mnt/data1/TZ/44/  -- 4123       
 ruby ./dady/bin/import_image.rb 4 45 3 /mnt/data1/TZ/45/  -- 12625
 ruby ./dady/bin/import_image.rb 4 46 3 /mnt/data1/TZ/46/  -- 6353
 ruby ./dady/bin/import_image.rb 4 47 3 /mnt/data1/TZ/47/  -- 4263
@@ -94,12 +94,12 @@ ruby ./dady/bin/import_image.rb 4 14 10 /mnt/data1/TZ/14/
 ruby ./dady/bin/import_image.rb 4 15 10 /mnt/data1/TZ/15/
 ruby ./dady/bin/import_image.rb 4 16 10 /mnt/data1/TZ/16/
 ruby ./dady/bin/import_image.rb 4 17 10 /mnt/data1/TZ/17/
-ruby ./dady/bin/import_image.rb 4 18 10 /mnt/data1/TZ/18/18\$E\$0041/ 41
-ruby ./dady/bin/import_image.rb 4 19 10 /mnt/data1/TZ/19/19\$E\$0679/ 679
+ruby ./dady/bin/import_image.rb 4 18 10 /mnt/data1/TZ/18/      18\$E\$0041/ 41
+ruby ./dady/bin/import_image.rb 4 19 10 /mnt/data1/TZ/19/      19\$E\$0679/ 679
 ruby ./dady/bin/import_image.rb 4 26 10 /mnt/data1/TZ/26/
 ruby ./dady/bin/import_image.rb 4 40 3 /mnt/data1/TZ/40/
 ruby ./dady/bin/import_image.rb 4 41 3 /mnt/data1/TZ/41/
-ruby ./dady/bin/import_image.rb 4 42 3 /mnt/data1/TZ/42/
+ruby ./dady/bin/import_image.rb 4 42 3 /mnt/data1/TZ/42/       495
 ruby ./dady/bin/import_image.rb 4 43 3 /mnt/data1/TZ/43/
 ruby ./dady/bin/import_image.rb 4 44 3 /mnt/data1/TZ/44/
 ruby ./dady/bin/import_image.rb 4 45 3 /mnt/data1/TZ/45/
@@ -140,3 +140,152 @@ ruby ./dady/bin/print_mulu_tj.rb 4 3 54
 ruby ./dady/bin/print_mulu_tj.rb 4 3 55
 ruby ./dady/bin/print_mulu_tj.rb 4 3 56
 
+
+
+====TZ2 统计===                              
+ruby ./dady/bin/print_mulu_tj.rb 4 10 10 
+ruby ./dady/bin/print_mulu_tj.rb 4 10 11 
+ruby ./dady/bin/print_mulu_tj.rb 4 14 21 
+ruby ./dady/bin/print_mulu_tj.rb 4 14 22 
+ruby ./dady/bin/print_mulu_tj.rb 4 14 27 
+ruby ./dady/bin/print_mulu_tj.rb 4 16 28 
+ruby ./dady/bin/print_mulu_tj.rb 4 19 29 
+ruby ./dady/bin/print_mulu_tj.rb 4 17 30 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  38 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  61 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  62 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  63 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  64 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  65 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  67 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  68 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  69 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  70 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  71 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  72 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  75 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  86 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  87 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  88 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  89 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  90 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  91 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  92 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  93 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  94 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  95 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  96 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  97 
+ruby ./dady/bin/print_mulu_tj.rb 4 4  98 
+ruby ./dady/bin/print_mulu_tj.rb 4  4 101
+ruby ./dady/bin/print_mulu_tj.rb 4  4 102
+ruby ./dady/bin/print_mulu_tj.rb 4  4 103
+ruby ./dady/bin/print_mulu_tj.rb 4  4 104
+ruby ./dady/bin/print_mulu_tj.rb 4  4 105
+ruby ./dady/bin/print_mulu_tj.rb 4  4 106
+ruby ./dady/bin/print_mulu_tj.rb 4  4 108
+ruby ./dady/bin/print_mulu_tj.rb 4  4 109
+ruby ./dady/bin/print_mulu_tj.rb 4  4 110
+ruby ./dady/bin/print_mulu_tj.rb 4  4 111
+ruby ./dady/bin/print_mulu_tj.rb 4  4 112
+ruby ./dady/bin/print_mulu_tj.rb 4 10 14 
+ruby ./dady/bin/print_mulu_tj.rb 4 10 15 
+ruby ./dady/bin/print_mulu_tj.rb 4 10 16 
+ruby ./dady/bin/print_mulu_tj.rb 4 10 17 
+ruby ./dady/bin/print_mulu_tj.rb 4 10 18 
+ruby ./dady/bin/print_mulu_tj.rb 4 10 19 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  40 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  41 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  42 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  43 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  44 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  45 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  46 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  47 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  48 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  49 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  50 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  51 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  52 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  53 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  54 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  55 
+ruby ./dady/bin/print_mulu_tj.rb 4 3  56 
+
+
+
+ruby ./dady/bin/import_image.rb 4 10 10 /mnt/lvm1/TZ2/10/  22197
+ruby ./dady/bin/import_image.rb 4 11 10 /mnt/lvm1/TZ2/11/
+ruby ./dady/bin/import_image.rb 4 21 14 /mnt/lvm1/TZ2/21/
+ruby ./dady/bin/import_image.rb 4 22 14 /mnt/lvm1/TZ2/22/
+ruby ./dady/bin/import_image.rb 4 27 14 /mnt/lvm1/TZ2/27/
+ruby ./dady/bin/import_image.rb 4 28 16 /mnt/lvm1/TZ2/28/
+ruby ./dady/bin/import_image.rb 4 29 19 /mnt/lvm1/TZ2/29/
+ruby ./dady/bin/import_image.rb 4 30 17 /mnt/lvm1/TZ2/30/
+ruby ./dady/bin/import_image.rb 4 38 4 /mnt/lvm1/TZ2/38/ 
+ruby ./dady/bin/import_image.rb 4 61 3 /mnt/lvm1/TZ2/61/ 
+                                                         
+ruby ./dady/bin/import_image.rb 4 62 3 /mnt/lvm1/TZ2/62/ 
+ruby ./dady/bin/import_image.rb 4 63 3 /mnt/lvm1/TZ2/63/ 
+ruby ./dady/bin/import_image.rb 4 64 3 /mnt/lvm1/TZ2/64/ 
+ruby ./dady/bin/import_image.rb 4 65 3 /mnt/lvm1/TZ2/65/ 
+ruby ./dady/bin/import_image.rb 4 67 4 /mnt/lvm1/TZ2/67/ 
+ruby ./dady/bin/import_image.rb 4 68 4 /mnt/lvm1/TZ2/68/ 
+ruby ./dady/bin/import_image.rb 4 69 4 /mnt/lvm1/TZ2/69/ 
+ruby ./dady/bin/import_image.rb 4 70 4 /mnt/lvm1/TZ2/70/ 
+ruby ./dady/bin/import_image.rb 4 71 4 /mnt/lvm1/TZ2/71/71\$C\$1287/ 1287 
+ruby ./dady/bin/import_image.rb 4 72 4 /mnt/lvm1/TZ2/72/ 
+                                                         
+ruby ./dady/bin/import_image.rb 4 75 4 /mnt/lvm1/TZ2/75/ 
+ruby ./dady/bin/import_image.rb 4 86 4 /mnt/lvm1/TZ2/86/ 
+ruby ./dady/bin/import_image.rb 4 87 4 /mnt/lvm1/TZ2/87/ 
+ruby ./dady/bin/import_image.rb 4 88 4 /mnt/lvm1/TZ2/88/ 
+ruby ./dady/bin/import_image.rb 4 89 4 /mnt/lvm1/TZ2/89/ 
+ruby ./dady/bin/import_image.rb 4 90 4 /mnt/lvm1/TZ2/90/ 
+ruby ./dady/bin/import_image.rb 4 91 4 /mnt/lvm1/TZ2/91/ 
+ruby ./dady/bin/import_image.rb 4 92 4 /mnt/lvm1/TZ2/92/ 
+ruby ./dady/bin/import_image.rb 4 93 4 /mnt/lvm1/TZ2/93/ 
+ruby ./dady/bin/import_image.rb 4 94 4 /mnt/lvm1/TZ2/94/ 
+
+ruby ./dady/bin/import_image.rb 4 95 4 /mnt/lvm1/TZ2/95/ -- 35547
+ruby ./dady/bin/import_image.rb 4 96 4 /mnt/lvm1/TZ2/96/ -- 31608
+ruby ./dady/bin/import_image.rb 4 97 4 /mnt/lvm1/TZ2/97/ -- 36364
+ruby ./dady/bin/import_image.rb 4 98 4 /mnt/lvm1/TZ2/98/ -- 19864
+ruby ./dady/bin/import_image.rb 4 101 4 /mnt/lvm1/TZ2/101/ -- 7057
+ruby ./dady/bin/import_image.rb 4 102 4 /mnt/lvm1/TZ2/102/ -- 31158
+ruby ./dady/bin/import_image.rb 4 103 4 /mnt/lvm1/TZ2/103/ -- 36270
+ruby ./dady/bin/import_image.rb 4 104 4 /mnt/lvm1/TZ2/104/ -- 37123
+ruby ./dady/bin/import_image.rb 4 105 4 /mnt/lvm1/TZ2/105/ -- 36437
+ruby ./dady/bin/import_image.rb 4 106 4 /mnt/lvm1/TZ2/106/ -- 
+
+ruby ./dady/bin/import_image.rb 4 108 4 /mnt/lvm1/TZ2/108/ --35286
+ruby ./dady/bin/import_image.rb 4 109 4 /mnt/lvm1/TZ2/109/ --33737
+ruby ./dady/bin/import_image.rb 4 110 4 /mnt/lvm1/TZ2/110/ 
+ruby ./dady/bin/import_image.rb 4 111 4 /mnt/lvm1/TZ2/111/ --33352
+ruby ./dady/bin/import_image.rb 4 112 4 /mnt/lvm1/TZ2/112/ --38243
+
+
+ruby ./dady/bin/import_image.rb 4 14 10 /mnt/data1/TZ/14/ -- 26248
+ruby ./dady/bin/import_image.rb 4 15 10 /mnt/data1/TZ/15/ -- 19624
+ruby ./dady/bin/import_image.rb 4 16 10 /mnt/data1/TZ/16/ -- 24551
+ruby ./dady/bin/import_image.rb 4 17 10 /mnt/data1/TZ/17/ -- 15845
+ruby ./dady/bin/import_image.rb 4 18 10 /mnt/data1/TZ/18/ -- 120065
+ruby ./dady/bin/import_image.rb 4 19 10 /mnt/data1/TZ/19/ -- 147180
+
+ruby ./dady/bin/import_image.rb 4 40 3 /mnt/data1/TZ/40/  -- 10745
+ruby ./dady/bin/import_image.rb 4 41 3 /mnt/data1/TZ/41/  -- 10604
+ruby ./dady/bin/import_image.rb 4 42 3 /mnt/data1/TZ/42/  -- 5608
+ruby ./dady/bin/import_image.rb 4 43 3 /mnt/data1/TZ/43/  -- 8919
+ruby ./dady/bin/import_image.rb 4 44 3 /mnt/data1/TZ/44/  -- 4123
+ruby ./dady/bin/import_image.rb 4 45 3 /mnt/data1/TZ/45/  -- 12625
+ruby ./dady/bin/import_image.rb 4 46 3 /mnt/data1/TZ/46/  -- 6353
+ruby ./dady/bin/import_image.rb 4 47 3 /mnt/data1/TZ/47/  -- 4263
+ruby ./dady/bin/import_image.rb 4 48 3 /mnt/data1/TZ/48/  -- 1011
+ruby ./dady/bin/import_image.rb 4 49 3 /mnt/data1/TZ/49/  -- 34514
+ruby ./dady/bin/import_image.rb 4 50 3 /mnt/data1/TZ/50/  -- 31579
+ruby ./dady/bin/import_image.rb 4 51 3 /mnt/data1/TZ/51/  -- 33970
+ruby ./dady/bin/import_image.rb 4 52 3 /mnt/data1/TZ/52/  -- 38300
+ruby ./dady/bin/import_image.rb 4 53 3 /mnt/data1/TZ/53/  -- 35118
+ruby ./dady/bin/import_image.rb 4 54 3 /mnt/data1/TZ/54/  -- 41771
+ruby ./dady/bin/import_image.rb 4 55 3 /mnt/data1/TZ/55/  -- 44651
+ruby ./dady/bin/import_image.rb 4 56 3 /mnt/data1/TZ/56/  -- 29023
