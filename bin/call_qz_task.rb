740efbf4741ee752adb7d2715debd85dbfbe389e
Liu Jun <szpapas@qq.com>
2012-03-28 08:45:24 +0800

    update print

--- bin/call_qz_task.rb
+++ bin/call_qz_task.rb
@@ -5,14 +5,14 @@ require 'pg'
 
 $conn = PGconn.open(:dbname=>'JY1017', :user=>'postgres', :password=>'brightechs', :host=>'localhost', :port=>'5432')
 
-user = $conn.exec("select * from q_status where dyzt='未开始' order by id;")
-
+user = $conn.exec("select * from q_status where zt='未开始' order by id;")
 $count = user.count
 
 while $count > 0 do 
   data = user[0]
   dh_prefix, mlh, cmd, fjcs = data['dhp'], data['mlh'],data['cmd'], data['fjcs']
   
+  $conn.exec("update q_status set zt='开始执行' where id=data['id];")
   puts "#{cmd}"
   system(cmd)
   
