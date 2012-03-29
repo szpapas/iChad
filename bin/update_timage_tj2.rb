44fb4af99645865abe164d4027afe27080ebe3ea
Liu Jun <szpapas@qq.com>
2012-03-29 16:10:52 +0800

    update timage_tj2

--- bin/update_timage_tj2.rb
+++ bin/update_timage_tj2.rb
@@ -18,7 +18,7 @@ dh = ARGV[0]
 $conn = PGconn.open(:dbname=>'JY1017', :user=>'postgres', :password=>'brightechs', :host=>'localhost', :port=>'5432')
 
 def update_timage(dh_prefix)
-  
+  $stderr.puts"更新 #{dh_prefix} ..."
   puts "update ML00..."
   $conn.exec"update timage_tj set smyx = (select count(*) from timage where timage.dh=timage_tj.dh and timage.yxbh similar to '[0..9]%') where timage_tj.dh_prefix='#{dh_prefix}';"
   
