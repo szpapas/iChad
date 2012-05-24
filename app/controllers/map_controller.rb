238b23f2c75c0a31220eb8daae9444dc06a6e9e9
szpapas <szpapas@qq.com>
2012-05-24 14:40:12 +0800

    local modify

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -171,7 +171,22 @@ class MapController < ApplicationController
     render :text =>"Success"
   end
   
+  #增加全部说明
+  def get_qztj
+    user = User.find_by_sql("select qzh, count(*) as mls, sum(zajh) as ajs, d_dwdm.dwdm from q_qzxx inner join d_dwdm on q_qzxx.qzh = d_dwdm.id group by qzh, d_dwdm.dwdm;")
+    render :text => user.to_json
+  end
   
+  
+  #0524 by Liu JUN
+  def report_zt
+    dh, zt = params['dh'], params['zt']
+    User.find_by_sql("update q_qzxx set zt='#{zt}' where dh_prefix='#{dh}';")
+    render :text => 'Success'
+  end
+  
+  
+  #Add by Wu 
   #通过RFID获取档案
   def wsRFID(strrfid)
     user=User.find_by_sql("select * from archive  where rfidstr=  '#{strrfid}';") 
@@ -250,33 +265,7 @@ class MapController < ApplicationController
     render :text => user.to_json
   end
   
-  #返回温湿度记录
-  def  WsWsd(strrq,intlx)
-    #'intxl 1代表返回指定日期的最后一个小时的温湿度记录，2代表返回指定日期的全部温湿度记录
 
-    if intlx==1
-      user= User.find_by_sql("select * from wsd  where rq='#{strrq}' order by sj  DESC;") 
-      size = user.size;
-      if size.to_i>0
-        user= User.find_by_sql("select * from wsd  where rq='#{strrq}' and sj='#{user[0]["sj"]}';")
-      end
-    else
-      user= User.find_by_sql("select * from wsd  where rq='#{strrq}' order by sj;") 
-    end
-    size = user.size;
-    if size.to_i > 0
-        text = "{results:#{size},rows:["          
-        for k in 0..user.size-1
-            text = text + user[k].to_json + ','
-        end
-        text = text[0..-2] + "]}"
-    else
-        text = "{results:0,rows:[]}"
-    end
-    render :text => text
-  end
-  
-  
   #通过RFID获取档案
   def get_archiveByRfid
     user=User.find_by_sql("select * from archive where rfidstr='#{params['rfidstr']}';") 
@@ -294,70 +283,7 @@ class MapController < ApplicationController
   end
   
   
-  #增加全部说明
-  def get_qztj
-    user = User.find_by_sql("select qzh, count(*) as mls, sum(zajh) as ajs, d_dwdm.dwdm from q_qzxx inner join d_dwdm on q_qzxx.qzh = d_dwdm.id group by qzh, d_dwdm.dwdm;")
-    render :text => user.to_json
-  end
-  
-  
-  #0524 by Liu JUN
-  def report_zt
-    dh, zt = params['dh'], params['zt']
-    User.find_by_sql("update q_qzxx set zt='#{zt}' where dh_prefix='#{dh}';")
-    render :text => 'Success'
-  end
-  
-  #返回温湿度记录
-  def  WsWsd
-    #'intxl 1代表返回指定日期的最后一个小时的温湿度记录，2代表返回指定日期的全部温湿度记录
 
-    if intlx==1
-      user= User.find_by_sql("select * from wsd  where rq='#{strrq}' order by sj  DESC;") 
-      size = user.size;
-      if size.to_i>0
-        user= User.find_by_sql("select * from wsd  where rq='#{strrq}' and sj='#{user[0]["sj"]}';")
-      end
-    else
-      user= User.find_by_sql("select * from wsd  where rq='#{strrq}' order by sj;") 
-    end
-    size = user.size;
-    if size.to_i > 0
-        text = "{results:#{size},rows:["          
-        for k in 0..user.size-1
-            text = text + user[k].to_json + ','
-        end
-        text = text[0..-2] + "]}"
-    else
-        text = "{results:0,rows:[]}"
-    end
-    render :text => text
-  end
-  
-  #上架
-  def wsRfidSetup
-    # 设置成功 传入参数不能为空 strRfid此标签不存在或此标签不是盒标签 设置失败在保存的时候报错 strrfid传入格式有问题
-    rq=Time.now.strftime("%Y-%m-%d")
-    strrid=params['strRfid'].split(';')
-    for k in 0..strrid.size-1
-      if strrid[k]!=''
-        strbox=strrid[k].split(',')
-        if strbox.size==4
-          if strbox[0]==""
-             User.find_by_sql("INSERT INTO bcerr (boxstr,ErrLx,psnID,rq) values ('#{strbox[1]}', 2,'#{strbox[3]}','#{rq}');") 
-          else  
-            User.find_by_sql("update archive set boxstr='#{strbox[1]}' where boxrfid= '#{strbox[0]}';") 
-            if strbox[2].to_i == 1
-              User.find_by_sql("INSERT INTO bcerr (boxstr,bcid,ErrLx,psnID,rq) values ('#{strbox[1]}', '#{strbox[0]}',1,'#{strbox[3]}','#{rq}');") 
-            else
-              User.find_by_sql("INSERT INTO bcerr (boxstr,bcid,ErrLx,psnID,rq) values ('#{strbox[1]}', '#{strbox[0]}',0,'#{strbox[3]}','#{rq}');") 
-            end
-          end  
-        end
-      end
-    end
-    render :text => "Success" 
-  end
   
   #通过RFID获取档案
   def wsRFID
@@ -376,144 +302,6 @@ class MapController < ApplicationController
     render :text => text
   end
   
-
-  #通过档号获取整目录档案  query 格式  qzh_dalb_mlh(文书处理24的mlh格式为 年度_机构问题号_保管期限 )
-  def wsAjToTxt
-
-    dh = params['query']
-    ss = params['query'].split('-')
-
-    
-    case (ss[1]) 
-			when "0"
-				user = User.find_by_sql("select * from archive where qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and mlh = '#{ss[2]}' order by ajh ;")
-			 
-			when "2"
-				user = User.find_by_sql("select archive.*,a_jhcw.pzqh,a_jhcw.pzzh,a_jhcw.jnzs,a_jhcw.fjzs from archive left join a_jhcw on archive.id=a_jhcw.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and mlh =  '#{ss[2]}'  order by ajh ;")
-				
-			when "3","5","6","7"
-				user = User.find_by_sql("select archive.*,a_tddj.djh,a_tddj.qlrmc,a_tddj.tdzl,a_tddj.qsxz,a_tddj.tdzh,a_tddj.tfh,a_tddj.ydjh from archive left join a_tddj on archive.id=a_tddj.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and mlh = '#{ss[2]}'  order by ajh ;")
-			when "15"
-				user = User.find_by_sql("select archive.*,a_sx.zl from archive left join a_sx on archive.id=a_sx.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and mlh =  '#{ss[2]}'  order by ajh ;")
-      when "18"
-				user = User.find_by_sql("select archive.*,a_tjml.tfh,a_tjml.tgh from archive left join a_tjml on archive.id=a_tjml.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and mlh =  '#{ss[2]}'  order by ajh ;")
-      when "25"
-				user = User.find_by_sql("select archive.*,a_dzda.tjr, a_dzda.rjhj, a_dzda.czxt, a_dzda.sl, a_dzda.bfs, a_dzda.ztbhdwjgs, a_dzda.yyrjpt, a_dzda.tjdw, a_dzda.wjzt, a_dzda.dzwjm, a_dzda.ztbh, a_dzda.xcbm, a_dzda.xcrq, a_dzda.jsr, a_dzda.jsdw, a_dzda.yjhj from archive left join a_dzda on archive.id=a_dzda.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and mlh =  '#{ss[2]}'  order by ajh ;")
-      when "27"
-				user = User.find_by_sql("select archive.*,a_sbda.zcmc, a_sbda.gzsj, a_sbda.dw, a_sbda.sl, a_sbda.cfdd, a_sbda.sybgdw, a_sbda.sybgr, a_sbda.jh, a_sbda.zcbh, a_sbda.dj, a_sbda.je from archive left join a_sbda on archive.id=a_sbda.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and mlh =  '#{ss[2]}' order by ajh ;")
-      when "26"
-				user = User.find_by_sql("select archive.*,a_jjda.xmmc, a_jjda.jsdw from archive left join a_jjda on archive.id=a_jjda.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and mlh =  '#{ss[2]}'  order by ajh ;")
-      when "28"
-				user = User.find_by_sql("select archive.*,a_swda.bh, a_swda.lb, a_swda.hjz, a_swda.sjsj, a_swda.sjdw, a_swda.mc, a_swda.ztxsfrom archive left join a_swda on archive.id=a_swda.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and mlh =  '#{ss[2]}'  order by ajh ;")
-      when "29"
-				user = User.find_by_sql("select archive.*,a_zlxx.bh, a_zlxx.lb, a_zlxx.bzdw from archive left join a_zlxx on archive.id=a_zlxx.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and mlh =  '#{ss[2]}'  order by ajh ;")
-      when "30"
-				user = User.find_by_sql("select archive.*,a_by_tszlhj.djh, a_by_tszlhj.kq, a_by_tszlhj.mc, a_by_tszlhj.fs, a_by_tszlhj.yfdw, a_by_tszlhj.cbrq, a_by_tszlhj.dj from archive left join a_by_tszlhj on archive.id=a_by_tszlhj.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}'   order by djh ;")
-      when "31"
-				user = User.find_by_sql("select archive.*,a_by_jcszhb.zt, a_by_jcszhb.qy, a_by_jcszhb.tjsj, a_by_jcszhb.sm from archive left join a_by_jcszhb on archive.id=a_by_jcszhb.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}'   order by zt ;")
-      when "32"
-				user = User.find_by_sql("select archive.*,a_by_zzjgyg.jgmc, a_by_zzjgyg.zzzc, a_by_zzjgyg.qzny from archive left join a_by_zzjgyg on archive.id=a_by_zzjgyg.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}'   order by jgmc ;")
-      when "33"
-				user = User.find_by_sql("select archive.*,a_by_dsj.dd, a_by_dsj.jlr, a_by_dsj.clly, a_by_dsj.fsrq, a_by_dsj.jlrq, a_by_dsj.rw, a_by_dsj.sy,a_by_dsj.yg from archive left join a_by_dsj on archive.id=a_by_dsj.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}'   order by fsrq ;")
-      when "34"
-				user = User.find_by_sql("select archive.*,a_by_qzsm.qzgcgjj, a_by_qzsm.sj from archive left join a_by_qzsm on archive.id=a_by_qzsm.ownerid where qzh = '#{ss[0]}' and dalb ='#{ss[1]}'   order by sj ;")
-      
-			when "24"
-			  #年度_机构问题号_保管期限
-			  case ss.length
-		      when 3
-		        strwhere="where archive.qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and a_wsda.nd='#{ss[2]}'"
-	        when 4
-	          strwhere="where archive.qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and a_wsda.nd='#{ss[2]}' and a_wsda.jgwth='#{ss[3]}'"
-          when 5
-            strwhere="where archive.qzh = '#{ss[0]}' and dalb ='#{ss[1]}' and a_wsda.nd='#{ss[2]}' and a_wsda.jgwth='#{ss[3]}' and a_wsda.bgqx='#{ss[4]}'"
-          else
-            strwhere="where archive.qzh = '#{ss[0]}' and dalb ='#{ss[1]}'"
-				    
-        end
-        user = User.find_by_sql("select archive.dwdm,archive.dh,archive.bz,archive.mlh,archive.flh,archive.id,archive.ys,archive.tm,archive.dalb,archive.qzh,a_wsda.jh,a_wsda.hh, a_wsda.zwrq, a_wsda.wh, a_wsda.zrr, a_wsda.gb, a_wsda.wz, a_wsda.ztgg, a_wsda.ztlx, a_wsda.ztdw, a_wsda.dagdh, a_wsda.dzwdh, a_wsda.swh, a_wsda.ztsl, a_wsda.qwbs, a_wsda.ztc, a_wsda.zbbm, a_wsda.ownerid, a_wsda.nd, a_wsda.jgwth, a_wsda.gbjh, a_wsda.xbbm, a_wsda.bgqx from archive left join a_wsda on archive.id=a_wsda.ownerid  #{strwhere}   order by nd,bgqx,jgwth,jh ;")
-			else
-				user = User.find_by_sql("select * from archive where dh like '#{dh}-%' order by ajh ;")
-				
-		end
-
-    size = user.size;
-    if size.to_i>0 
-      txt = "{results:100,rows:["
-      for k in 0..user.size-1
-          txt = txt + user[k].to_json + ','
-      end
-      txt = txt[0..-2] + "]}"
-    else
-      txt = "{results:0,rows:[]}"
-    end
-
-    render :text => user.to_json
-
-  end
-  
-  #返回温湿度记录
-  def  WsWsd(strrq,intlx)
-    #'intxl 1代表返回指定日期的最后一个小时的温湿度记录，2代表返回指定日期的全部温湿度记录
-
-    if intlx==1
-      user= User.find_by_sql("select * from wsd  where rq='#{strrq}' order by sj  DESC;") 
-      size = user.size;
-      if size.to_i>0
-        user= User.find_by_sql("select * from wsd  where rq='#{strrq}' and sj='#{user[0]["sj"]}';")
-      end
-    else
-      user= User.find_by_sql("select * from wsd  where rq='#{strrq}' order by sj;") 
-    end
-    size = user.size;
-    if size.to_i > 0
-        text = "{results:#{size},rows:["          
-        for k in 0..user.size-1
-            text = text + user[k].to_json + ','
-        end
-        text = text[0..-2] + "]}"
-    else
-        text = "{results:0,rows:[]}"
-    end
-    render :text => text
-  end
-  
-
-  #获取影像文件列表
-  def get_imageList
-    user= User.find_by_sql("select id, yxmbh from timage where dh='#{params['dh']}' order by yxbh;") 
-    size = user.size;
-    if size.to_i > 0
-        text = "{results:#{size},rows:["          
-        for k in 0..user.size-1
-            text = text + user[k].to_json + ','
-        end
-        text = text[0..-2] + "]}"
-    else
-        text = "{results:0,rows:[]}"
-    end
-    render :text => text
-  end
-  
-  #上架
-  def wsRfidSetup(strrfid)    
-    rq=Time.now.strftime("%Y-%m-%d")
-    strrid=strrfid.split(';')
-    for k in 0..strrid.length-1
-      if strrid[k]!=''
-        strbox=strrid[k].split(',')
-        if strbox.length==4
-          User.find_by_sql("update archive set boxstr='#{strbox[1]}' where rfidstr=  '#{strbox[0]}';") 
-          if strbox[2] == 1
-            User.find_by_sql("INSERT INTO bcerr (boxstr,bcid,ErrLx,psnID,rq) values ('#{strbox[1]}', '#{strbox[0]}',1,'#{strbox[3]}','#{rq}');") 
-          else
-            User.find_by_sql("INSERT INTO bcerr (boxstr,bcid,ErrLx,psnID,rq) values ('#{strbox[1]}', '#{strbox[0]}',0,'#{strbox[3]}','#{rq}');") 
-          end
-        end
-      end
-    end
-    render :text => "success" 
-  end
 
   
   #通过RFID获取档案
@@ -804,24 +592,5 @@ class MapController < ApplicationController
     end
     render :text => text
   end
-  def wsPK    
-    #query格式：rfidstr,档案ID,zt(0/1),所在盒boxstr,档案类别,人员ID,错误说明;rfidstr,档案ID,zt(0/1),所在盒boxstr,档案类别,人员ID,错误说明
-    if params['query']!=""
-        rq=Time.now.strftime("%Y-%m-%d")
-        strrfid = params['query'].split(';')
-        for k in 0..strrfid.size-1
-          if strrfid[k]!=""
-            strbox=strrfid[k].split(',')
-            if strbox[0]!=""                       
-              user = User.find_by_sql("INSERT INTO pkerr (bcid,gsboxstr,szboxstr,DaID,ErrLx,psnID,dalb,rq,cs,sj) values ('#{strbox[0]}', '#{strbox[3]}', '#{strbox[4]}', '#{strbox[1]}','#{strbox[2]}', '#{strbox[6]}', '#{strbox[5]}','#{rq}', '#{strbox[7]}', '#{strbox[8]}');")
-            end
-          end
-          
-        end
-    end
-                    
-    render :text =>"success"
-  end
-  
   
 end
